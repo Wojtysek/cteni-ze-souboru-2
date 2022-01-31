97989f8a241e77513d21ea161877db707625963b
Wojtysek <vojtahr1@post.cz>
2022-01-31 13:43:27 +0100

    funkcni

--- cteni ze souboru/cteni ze souboru.cpp
+++ cteni ze souboru/cteni ze souboru.cpp
@@ -4,20 +4,21 @@ int main(void) {
 
 	FILE* vstup = fopen("znamky.txt", "r");
 
-	if (vstup = NULL) {
+	if (vstup == NULL) {
 		printf("ERROR:nejde otevrit.");
 		return 1;
 	}
 	while (!feof(vstup)) {
 		float prumer = 0;
 		char jmeno[20];
-		int znamky[8];
+		int znamky;
 		fscanf(vstup, "%s", &jmeno);
 		for (int i = 0; i <= 8; i++) {
-			fscanf(vstup, "%d", &znamky[i]);
-				prumer += znamky[i];
+			fscanf(vstup, "%d", &znamky);
+			prumer += znamky;
 		}
-		prumer /= 10;
+		prumer /= 8;
 		printf("%s: %7.2f\n", jmeno, prumer);
+		printf("\n");
 	}
 }
