4f9496f63ac50967d9a62acf6b7190ebceecfc86
Wojtysek <vojtahr1@post.cz>
2022-01-31 13:29:07 +0100

    Přidejte soubory projektu.

---
+++ cteni ze souboru/cteni ze souboru.cpp
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+
+int main(void) {
+
+	FILE* vstup = fopen("znamky.txt", "r");
+
+	if (vstup = NULL) {
+		printf("ERROR:nejde otevrit.");
+		return 1;
+	}
+	while (!feof(vstup)) {
+		float prumer = 0;
+		char jmeno[20];
+		int znamky[8];
+		fscanf(vstup, "%s", &jmeno);
+		for (int i = 0; i <= 8; i++) {
+			fscanf(vstup, "%d", &znamky[i]);
+				prumer += znamky[i];
+		}
+		prumer /= 10;
+		printf("%s: %7.2f\n", jmeno, prumer);
+	}
+}
